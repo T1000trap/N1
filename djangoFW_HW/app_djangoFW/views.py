d426a0c2df64300b4062c75afff1ecd180bd7597
Leonid <dildin_leonid@mail.ru>
2023-07-20 15:46:49 +0300

    New, new, new, new homework

--- djangoFW_HW/app_djangoFW/views.py
+++ djangoFW_HW/app_djangoFW/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("successfull")
+    return HttpResponse(request, "index.html")
+
+def top_sellers(requests):
+    return render(requests, "top-sellers.html")
--- djangoFW_HW/djangoFW/settings.py
+++ djangoFW_HW/djangoFW/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'djangoFW.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,5 +121,9 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+STATICFILES_DIRS = [
+    Path.joinpath(BASE_DIR, "static"),
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

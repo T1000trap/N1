cc4958137d3bbafa3d6b92f7d514852e0d53de69
Leonid <dildin_leonid@mail.ru>
2023-08-10 20:31:41 +0300

    New, new, new, new, new, new homework

--- djangoFW_HW/app_djangoFW/models.py
+++ djangoFW_HW/app_djangoFW/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class Advertisment(models.Model):
     title = models.CharField("Заголовок", max_length=128)
@@ -9,6 +14,7 @@ class Advertisment(models.Model):
     auction = models.BooleanField("Торг", help_text="Будет, ли торг")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
 
 
     @admin.display(description="дата создания")
--- djangoFW_HW/app_djangoFW/views.py
+++ djangoFW_HW/app_djangoFW/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
+from .models import Advertisment
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse(request, "index.html")
+    advertisments =  Advertisment.objects.all()
+    context = {"advertisments" : advertisments}
+    return render(request, "index.html", context)
 
 def top_sellers(requests):
     return render(requests, "top-sellers.html")
